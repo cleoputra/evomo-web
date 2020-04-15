a7858e612718a684bd92b05c1892cea02af8be43
cleoputra <acesam222@gmail.com>
2020-04-15 13:58:19 +0700

    revisi error deploy

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const NotFound = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh",
+    }}
+  >
+    <h1>404 Not Found!</h1>
+  </div>
+)
+
+export default NotFound
